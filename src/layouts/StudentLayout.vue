ffcc53e4ac0969f03c05c4f273006fd243158118
Walter Tapia <tapiaw38@gmail.com>
2026-09-22 19:58:51 -0300

    feat(student): add league to sidebar

--- src/layouts/StudentLayout.vue
+++ src/layouts/StudentLayout.vue
@@ -258,6 +258,16 @@
         </RouterLink>
 
         <RouterLink
+          to="/student/league"
+          class="nav-item"
+          active-class="nav-item-active"
+          @click="navOpen = false"
+        >
+          <span class="nav-icon"><i class="pi pi-bolt"></i></span>
+          <span>Mi liga</span>
+        </RouterLink>
+
+        <RouterLink
           to="/student/profile"
           class="nav-item"
           active-class="nav-item-active"
